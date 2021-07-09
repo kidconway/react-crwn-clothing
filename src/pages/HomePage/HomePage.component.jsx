01436eea31d5ccedb30a43e4292ed0d9ccc0debc
kidconway <colbyhoran@gmail.com>
2021-07-08 19:53:33 -0500

    broken homepage into smaller components, finished styling menu components

---
+++ src/pages/HomePage/HomePage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./HomePage.styles.scss";
+
+import Directory from "../../components/Directory/Directory.component";
+
+const HomePage = props => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
