1eb400cf0294e6cb132190b5a72b21caa0d2ba88
kidconway <colbyhoran@gmail.com>
2021-07-10 21:16:38 -0500

    finishing login handle submit method

--- src/components/Login/Login.component.jsx
+++ src/components/Login/Login.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../FormInput/FormInput.component";
 import Button from "../Button/Button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./Login.style.scss";
 
@@ -17,10 +17,17 @@ export default class Login extends React.Component {
     };
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (err) {
+      console.log("Login component ", err.message);
+    }
   };
 
   handleChange = e => {
